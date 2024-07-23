d041938b0167666ba9bac7b8a59a7e30b2398b20
ShivaKanchi <shivakanchi111@gmail.com>
2024-07-24 00:17:30 +0530

    context created

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
-import SignIn from "./components/auth/SignIn";
+import { Container } from "react-bootstrap";
+import SignUp from "./components/auth/SignUp";
+import { AuthProvider } from "./contexts/AuthContext";
 
 function App() {
-  return <SignIn />;
+  return (
+    <AuthProvider>
+      <Container
+        className="d-flex align-items-center justify-content-center"
+        style={{ minHeight: "100vh" }}
+      >
+        <div className="w-100" style={{ maxWidth: "400px" }}>
+          <SignUp />
+        </div>
+      </Container>
+    </AuthProvider>
+  );
 }
 
 export default App;
