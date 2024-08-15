948308a641bd9ba75df7c4d78e200f86b35b0b69
ShivaKanchi <shivakanchi111@gmail.com>
2024-08-15 23:03:43 +0530

    buttons wip

---
+++ src/components/drive/AddFolderButton.jsx
@@ -0,0 +1,10 @@
+import { Button } from "react-bootstrap";
+
+export default function AddFolderButton() {
+  function openModal() {}
+  return (
+    <Button onClick={openModal} variant="outline-success" size="sm">
+      Open
+    </Button>
+  );
+}
--- src/components/drive/Dashboard.jsx
+++ src/components/drive/Dashboard.jsx
@@ -1,9 +1,12 @@
+import { Container } from "react-bootstrap";
 import { HeaderComponent } from "../HeaderComponent";
 
 export default function Dashboard() {
   return (
     <>
       <HeaderComponent />
+
+      <Container fluid>Content</Container>
     </>
   );
 }
