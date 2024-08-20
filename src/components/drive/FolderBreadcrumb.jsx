b943bd2d9a66b16a1a4895ca2df4e48a8184a3a3
ShivaKanchi <shivakanchi111@gmail.com>
2024-08-21 00:00:04 +0530

    breadcrumb wip

--- src/components/drive/FolderBreadcrumb.jsx
+++ src/components/drive/FolderBreadcrumb.jsx
@@ -1,3 +1,16 @@
-export default function FolderBreadcrumb() {
-  return <div className="d-flex align-items-center">FolderBreadcrumb</div>;
+import { Breadcrumb } from "react-bootstrap";
+
+export default function FolderBreadcrumb({ currentFolder }) {
+  return (
+    <div className="d-flex align-items-center">
+      <Breadcrumb
+        className="flex-grow-1 "
+        listProps={{ className: "bg-white p-0 m-0" }}
+      >
+        {currentFolder && (
+          <Breadcrumb.Item>{currentFolder.name}</Breadcrumb.Item>
+        )}
+      </Breadcrumb>
+    </div>
+  );
 }
